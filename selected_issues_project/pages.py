f9331615d6e635e29bc2d7f03d9bf33601292379
MarwanAbs <85881565+MarwanAbs@users.noreply.github.com>
2021-06-18 17:39:17 +0300

    Update pages.py

--- selected_issues_project/pages.py
+++ selected_issues_project/pages.py
@@ -2,7 +2,7 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 import random
-import numpy as np
+
 
 
 class Informedconsent(Page):
